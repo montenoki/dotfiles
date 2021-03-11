87b352fe55e8ed61fb68eb3dddee02cb86fb2a10
montenoki <monten.oki@gmail.com>
2021-03-11 18:41:49 +0900

    .zshrc_gcp

--- ubuntu_gcp.sh
+++ ubuntu_gcp.sh
@@ -7,7 +7,7 @@ sudo apt install curl git zsh bat python3-pip -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s /bin/zsh
 
-cd ~ && wget https://raw.githubusercontent.com/montenoki/auto_setup/main/.zshrc -O .zshrc
+cd ~ && wget https://raw.githubusercontent.com/montenoki/auto_setup/main/.zshrc_gcp -O .zshrc
 
 python3 -m pip install --upgrade pip
 
