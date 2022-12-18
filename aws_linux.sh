7dbe30be3f3bf37a5afb26cbc718d1d3a2fa32c7
montenoki <61886293+montenoki@users.noreply.github.com>
2022-12-18 23:21:25 +0900

    Update aws_linux.sh

--- aws_linux.sh
+++ aws_linux.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 sudo yum install git
 # ====================
@@ -18,4 +19,19 @@ echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 exec $SHELL -l
 # 安装Python
 pyenv install 3.11.1
+pyenv global 3.11.1
 
+# ====================
+# 安装Neo Vim
+# 参考：https://gist.github.com/kawaz/393c7f62fe6e857cc3d9
+# ====================
+sudo yum groups install -y Development\ tools
+sudo yum install -y cmake
+sudo pip install neovim --upgrade
+(
+cd "$(mktemp -d)"
+git clone https://github.com/neovim/neovim.git
+cd neovim
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+)
