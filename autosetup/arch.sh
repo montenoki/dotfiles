1660d2b3f34767c755ed2f4653f7373bde5e7eef
monten.oki <monten.oki@gmail.com>
2023-06-20 22:12:56 +0900

    fix

--- autosetup/arch.sh
+++ autosetup/arch.sh
@@ -4,35 +4,48 @@
 #                      User Interface                      #
 ############################################################
 
-read -p  "Do you wish to create a new user (y/n)?" -a yn
+read -p "Do you wish to create a new user (y/n)?" -a yn
 case $yn in
-[Yy]* )
-    read -p "Enter new user name:" user_name
-    echo "Enter new passwd for $user_name:"
-    read -s user_passwd
-    echo "Retype new passwd:"
-    read -s user_passwd_confirm
-    if [ $user_passwd != $user_passwd_confirm ]; then
-        echo "Sorry, passwords do not match."
-        echo "Exit autosetup script..."
-        exit 1
-    fi
-    ;;
-[Nn]* )
-    read -p "Enter a current user name for installation:" -a user_name
-    ;;
-*) exit;;
+[Yy]*)
+	read -p "Enter new user name:" user_name
+	echo "Enter new passwd for $user_name:"
+	read -s user_passwd
+	echo "Retype new passwd:"
+	read -s user_passwd_confirm
+	if [ $user_passwd != $user_passwd_confirm ]; then
+		echo "Sorry, passwords do not match."
+		echo "Exit autosetup script..."
+		exit 1
+	fi
+	$enable_create_user=true
+	;;
+[Nn]*)
+	read -p "Enter a current user name for installation:" -a user_name
+	$enable_create_user=false
+	;;
+*) exit ;;
 esac
 
 read -p "Do you wish to install Nushell (y/n)?" -a yn
 case $yn in
-    [Yy]*)
-        $enable_nushell=true
-		;;
-	[Nn]*)
-		$enable_nushell=false
-		;;
-    *) exit;;
+[Yy]*)
+	$enable_nushell=true
+	;;
+[Nn]*)
+	$enable_nushell=false
+	;;
+*) exit ;;
+esac
+
+read -p "Do you wish to install Starship (y/n)?" -a yn
+case $yn in
+[Yy]*)
+	$enable_starship=true
+	;;
+[Nn]*)
+	$enable_starship=false
+	;;
+*) exit ;;
 esac
 
 ############################################################
@@ -46,15 +59,16 @@ pacman -Syu --noconfirm
 #                      Creat New User                      #
 ############################################################
 
-echo "New User creating..."
-useradd -m -G wheel -s /bin/bash $user_name
-(
-	echo $user_passwd_confirm
-	sleep 1
-	echo $user_passwd
-) | passwd $user_name
-
-sed -e 's/^#\s\(%wheel\s\+ALL=(ALL:ALL)\s\+ALL\)/\1/' /etc/sudoers | EDITOR=tee visudo >/dev/null
+if [ $enable_create_user=true ]; then
+	echo "New User creating..."
+	useradd -m -G wheel -s /bin/bash $user_name
+	(
+		echo $user_passwd_confirm
+		sleep 1
+		echo $user_passwd
+	) | passwd $user_name
+	sed -e 's/^#\s\(%wheel\s\+ALL=(ALL:ALL)\s\+ALL\)/\1/' /etc/sudoers | EDITOR=tee visudo >/dev/null
+fi
 
 ############################################################
 #                  Software Installations                  #
@@ -70,12 +84,12 @@ pacman -S git bat bottom du fd gitui lsd ripgrep sd tealdeer zoxide --noconfirm
 echo "Cargo ..."
 sudo -u $user_name sh <<EOF
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-source "$HOME/.cargo/env"
 EOF
 
 if [ $enable_nushell=true ]; then
 	echo "Nushell ..."
 	sudo -u $user_name sh <<EOF
+source "$HOME/.cargo/env"
 cargo install nu --features=dataframe
 sed -e "/home/$user_name/.cargo/bin/nu" /etc/shells
 chsh /home/$user_name/.cargo/bin/nu
@@ -83,11 +97,16 @@ EOF
 fi
 
 if [ $enable_starship=true ]; then
-    echo "Starship ..."
-sudo -u $user_name sh <<EOF
+	echo "Starship ..."
+	sudo -u $user_name sh <<EOF
 curl https://starship.rs/install.sh | sh
 sed -e 'eval "$(starship init bash)"' ~/.bashrc
+EOF
+	if [ $enable_nushell=true ]; then
+		sudo -u $user_name sh <<EOF
 mkdir ~/.cache/starship
 starship init nu | save -f ~/.cache/starship/init.nu
 sed -e 'source ~/.cache/starship/init.nu' $nu.config-path
 EOF
+	fi
+fi
