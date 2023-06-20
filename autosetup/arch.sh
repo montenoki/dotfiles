314da2ba15086c2e5105db270707fc42c0671fc1
monten.oki <monten.oki@gmail.com>
2023-06-20 15:03:00 +0900

    feat: 目录整理, 添加arch.sh

---
+++ autosetup/arch.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+############################################################
+#                      User Interface                      #
+############################################################
+
+read -p "Enter new user name:" user_name
+echo "Enter new passwd for $user_name:"
+read -s user_passwd
+echo "Retype new passwd:"
+read -s user_passwd_confirm
+
+if [ $user_passwd != $user_passwd_confirm ]; then
+    echo "Sorry, passwords do not match."
+    echo "Exit autosetup script..."
+    exit 1
+fi
+
+############################################################
+#                      Update System                       #
+############################################################
+
+echo "System Updating..."
+pacman -Syu --noconfirm
+
+############################################################
+#                      Creat New User                      #
+############################################################
+
+echo "New User creating..."
+wheel附加组可sudo, 以root用户执行命令; -m同时创建$HOME
+useradd -m -G wheel -s /bin/bash $user_name
+(
+    echo $user_passwd_confirm
+    sleep 1
+    echo $user_passwd
+) | passwd $user_name
+
+sed -e 's/^#\s\(%wheel\s\+ALL=(ALL:ALL)\s\+ALL\)/\1/' /etc/sudoers | EDITOR=tee visudo >/dev/null
+
+############################################################
+#                  Software Installations                  #
+############################################################
+
+echo "Software Installing..."
+
+curl https://sh.rustup.rs -sSf | sh
+# pacman -S git
+
