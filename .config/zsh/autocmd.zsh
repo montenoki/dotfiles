011e0b46bca230b14a8e3d071b2260746d0968b3
monten.oki <monten.oki@gmail.com>
2024-02-24 18:00:25 +0900

    fix

--- .config/zsh/autocmd.zsh
+++ .config/zsh/autocmd.zsh
@@ -4,7 +4,9 @@ proc_count=`ps -ef |grep -w $proc_name|grep -v grep|wc -l`
 if [ $proc_count -le 0 ];then
   eval `ssh-agent`
 else
-   echo $proc_name is  running..
+  echo $proc_name is  running..
+  ps -axf | grep ssh-agent | grep -v grep | awk '{print "kill -9 " $1}' | zsh
+  eval `ssh-agent`
 fi
 for key in ~/.ssh/id_ed25519; do
 	if ! ssh-add -l | grep -q "$(ssh-keygen -lf "$key" | cut -d \  -f 2)"; then
--- .zlogout
+++
@@ -1,1 +0,0 @@
-ps -axf | grep ssh-agent | grep -v grep | awk '{print "kill -9 " $1}' | zsh
