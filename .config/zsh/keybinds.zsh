080c458f199101df487accba2e855f5fa88bc936
monten.oki <monten.oki@gmail.com>
2024-05-09 13:29:21 +0900

    fix zsh keybinds

--- .config/zsh/keybinds.zsh
+++ .config/zsh/keybinds.zsh
@@ -2,8 +2,10 @@
 
 bindkey "^[[H" beginning-of-line
 bindkey "^[[F" end-of-line
+bindkey "^[[1~" beginning-of-line
+bindkey "^[[4~" end-of-line
 
 bindkey "^[[3~" delete-char
 
-bindkey "^[[1;C" forward-word
-bindkey "^[[1;D" backward-word
+# bindkey "^[[1;C" forward-word
+# bindkey "^[[1;D" backward-word
