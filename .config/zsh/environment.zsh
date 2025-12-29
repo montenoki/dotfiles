61925cfc16a8ea18b736b6496c2ae5db2b6f5a76
monten.oki <monten.oki@gmail.com>
2025-12-28 22:49:43 -0500

    添加 nvm nodejs 虚拟环境管理

--- .config/zsh/environment.zsh
+++ .config/zsh/environment.zsh
@@ -88,6 +88,13 @@ if [[ -e "$HOME/.cargo/env" ]]; then
     . "$HOME/.cargo/env"
 fi
 
+# Node Version Manager (nvm)
+export NVM_DIR="$HOME/.config/nvm"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    . "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+fi
+
 # OpenCode
 path_add --prepend "$HOME/.opencode/bin"
 
